7eed1f4c9185dda6b81d8dae7f8ccb585d2e4074
Marthely <marthely.tsiba@arep.fr>
2024-12-10 10:40:48 +0100

    home map slider

--- js/accueil.js
+++ js/accueil.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
   bindHoverEffects("#frugacite-accronym span", "#frugacite-definition span");
+
+  // Select all map sliders
+  $(".map_slider").each(function () {
+    const slider = $(this); // Current slider instance
+
+    slider.find(".hoverer").on("mouseenter", function () {
+      slider.find(".map-bottom-div").addClass("show");
+    });
+    slider.find(".hoverer").on("mouseleave", function () {
+      slider.find(".map-bottom-div").removeClass("show");
+    });
+
+    // Click event for the right button
+    slider.find(".map-right-button").on("click", function () {
+      // Slide in the sliding div for the current slider
+      slider.find(".map-sliding-div").toggleClass("show");
+    });
+  });
 });
