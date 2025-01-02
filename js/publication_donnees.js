2aedecb913dc5af63e2b7c3f3a73a19a05d26c85
anettle <72600317+anettle@users.noreply.github.com>
2025-01-02 11:33:23 +0100

    Update publication_donnees.js

--- js/publication_donnees.js
+++ js/publication_donnees.js
@@ -1,22 +1,28 @@
 $(document).ready(function () {
-  // Set up the initial state for the icons on page load
-  $(".card-header .btn span").each(function() {
-    var content = $(this).closest(".card-header").next(".collapse");
-    if (!content.hasClass("show")) {
-      $(this).text("+");
-    }
-  });
+  // TOGGLE FOR CARD HEADER (toggle collapsible sections)
+  $(".card-header").on("click", function () {
+    var content = $(this).next(".collapse");  // Get the collapsible content that follows the card header
+    var btnSpan = $(this).find("button span");  // Get the span inside the button (to toggle + / -)
 
-  // Listen for the Bootstrap collapse events to change the icon
-  $(".btn[data-bs-toggle='collapse']").on("click", function () {
-    var content = $($(this).attr("data-bs-target"));  // Get the collapsible content
-    var btnSpan = $(this).find("span");  // Get the span inside the button (to toggle + / -)
+    // Toggle the visibility of the collapsible content
+    content.toggleClass("show");  // This will toggle the 'show' class to expand/collapse the section
 
-    // Check the collapse state and toggle the icon
+    // Toggle the + / - icon based on visibility
     if (content.hasClass("show")) {
       btnSpan.text("-");  // Show "-" when expanded
     } else {
       btnSpan.text("+");  // Show "+" when collapsed
+    }
+  });
+
+  // Initialize all sections to be collapsed with the correct + icon
+  $(".card-header").each(function() {
+    var content = $(this).next(".collapse");
+    var btnSpan = $(this).find("button span");
+
+    // Make sure the icon is "+" for collapsed sections
+    if (!content.hasClass("show")) {
+      btnSpan.text("+");
     }
   });
 });
