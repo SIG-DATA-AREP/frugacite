3c35d0c207160d5a9c6e10ea8ce2556aed30a764
anettle <72600317+anettle@users.noreply.github.com>
2025-01-02 14:26:59 +0100

    Update integrer.js

--- js/integrer.js
+++ js/integrer.js
@@ -1,28 +1,8 @@
 $(document).ready(function () {
-  // TOGGLE FOR CARD HEADER
+  // TOOGLE FOR CARD HEADER
   $(".card-header").on("click", function () {
-    var span = $(this).find(".btn span"); // Get the span inside the button
-
-    // Check if the collapse content is currently expanded
-    if ($(this).next(".collapse").hasClass("show")) {
-      // If expanded, add the 'hide' class to show the "-" symbol
-      span.addClass("hide");
-    } else {
-      // If collapsed, remove the 'hide' class to show the "+" symbol
-      span.removeClass("hide");
-    }
-  });
-
-  // Initialize all sections to be collapsed with the "+" symbol
-  $(".card-header").each(function() {
-    var span = $(this).find(".btn span");  // Get the span inside the button
-    var content = $(this).next(".collapse");
-
-    // Make sure the icon is "+" for collapsed sections
-    if (!content.hasClass("show")) {
-      span.removeClass("hide");  // "+" when collapsed
-    } else {
-      span.addClass("hide");  // "-" when expanded
-    }
+    if ($(this).find(".btn span").hasClass("hide"))
+      $(this).find(".btn span").removeClass("hide");
+    else $(this).find(".btn span").addClass("hide");
   });
 });
