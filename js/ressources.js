c5c088fb8bbe4badb03d62a50fe9453216caf8a0
anettle <72600317+anettle@users.noreply.github.com>
2025-01-02 11:53:30 +0100

    Update ressources.js Toggling

--- js/ressources.js
+++ js/ressources.js
@@ -1,8 +1,28 @@
 $(document).ready(function () {
-  // TOOGLE FOR CARD HEADER
+  // TOGGLE FOR CARD HEADER
   $(".card-header").on("click", function () {
-    if ($(this).find(".btn span").hasClass("hide"))
-      $(this).find(".btn span").removeClass("hide");
-    else $(this).find(".btn span").addClass("hide");
+    var span = $(this).find(".btn span"); // Get the span inside the button
+
+    // Check if the collapse content is currently expanded
+    if ($(this).next(".collapse").hasClass("show")) {
+      // If expanded, add the 'hide' class to show the "-" symbol
+      span.addClass("hide");
+    } else {
+      // If collapsed, remove the 'hide' class to show the "+" symbol
+      span.removeClass("hide");
+    }
+  });
+
+  // Initialize all sections to be collapsed with the "+" symbol
+  $(".card-header").each(function() {
+    var span = $(this).find(".btn span");  // Get the span inside the button
+    var content = $(this).next(".collapse");
+
+    // Make sure the icon is "+" for collapsed sections
+    if (!content.hasClass("show")) {
+      span.removeClass("hide");  // "+" when collapsed
+    } else {
+      span.addClass("hide");  // "-" when expanded
+    }
   });
 });
