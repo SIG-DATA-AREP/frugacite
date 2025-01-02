5e0aacb6d42b720979924b41c47072021528a494
anettle <72600317+anettle@users.noreply.github.com>
2025-01-02 10:31:58 +0100

    Update publication_donnees.js Test toggle sections

--- js/publication_donnees.js
+++ js/publication_donnees.js
@@ -1,8 +1,17 @@
 $(document).ready(function () {
-  // TOOGLE FOR CARD HEADER
+  // TOGGLE FOR CARD HEADER (toggle collapsible sections)
   $(".card-header").on("click", function () {
-    if ($(this).find(".btn span").hasClass("hide"))
-      $(this).find(".btn span").removeClass("hide");
-    else $(this).find(".btn span").addClass("hide");
+    var content = $(this).next(".collapse");  // Get the collapsible content that follows the card header
+    var btnSpan = $(this).find(".btn span");  // Get the span inside the button (to toggle + / -)
+
+    // Toggle the visibility of the collapsible content
+    content.toggleClass("show");  // This will toggle the 'show' class to expand/collapse the section
+
+    // Toggle the + / - icon based on visibility
+    if (content.hasClass("show")) {
+      btnSpan.removeClass("hide");  // Show "-" (expanded state)
+    } else {
+      btnSpan.addClass("hide");  // Show "+" (collapsed state)
+    }
   });
 });
