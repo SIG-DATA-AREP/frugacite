3ac56ed0a57f0312cb9a65a856d7ef0ae2417e62
anettle <72600317+anettle@users.noreply.github.com>
2025-01-03 09:59:12 +0100

    Update integrer.js add slider to java script

--- js/integrer.js
+++ js/integrer.js
@@ -5,4 +5,67 @@ $(document).ready(function () {
       $(this).find(".btn span").removeClass("hide");
     else $(this).find(".btn span").addClass("hide");
   });
+  // Sélection de tous les sliders 01
+  $("#page_integrer .mapSliderContainer.01 .map_slider").each(
+    function () {
+      const slider = $("#page_integrer .mapSliderContainer.03"); // Instance du slider actuel
+
+      // Gestion de la navigation entre les sliders
+      var $sliders = $(
+        "#page_integrer .mapSliderContainer.03 .map_slider"
+      );
+      var $prevBtn = slider.find(".prevBtn");
+      var $nextBtn = slider.find(".nextBtn");
+      var $indicator = slider.find(".sliderIndicator"); // L'élément qui affiche l'indicateur
+      var total = $sliders.length;
+      var currentIndex = 0; // Commence au premier slider
+
+      // Cacher tous les sliders et afficher le premier
+      $sliders.hide().eq(currentIndex).show();
+      updateButtonState();
+
+      function showSlider(index) {
+        $sliders.stop(true, true).fadeOut(300);
+        $sliders
+          .eq(index)
+          .stop(true, true)
+          .fadeIn(300, function () {
+            updateButtonState();
+          });
+      }
+
+      function updateButtonState() {
+        // Gestion de l'état du bouton "Précédent"
+        if (currentIndex === 0) {
+          $prevBtn.addClass("disabled");
+        } else {
+          $prevBtn.removeClass("disabled");
+        }
+
+        // Gestion de l'état du bouton "Suivant"
+        if (currentIndex === $sliders.length - 1) {
+          $nextBtn.addClass("disabled");
+        } else {
+          $nextBtn.removeClass("disabled");
+        }
+
+        // Mise à jour de l'indicateur "currentIndex/total"
+        $indicator.text(currentIndex + 1 + "/" + total);
+      }
+
+      $prevBtn.click(function () {
+        if (currentIndex > 0) {
+          currentIndex--;
+          showSlider(currentIndex);
+        }
+      });
+
+      $nextBtn.click(function () {
+        if (currentIndex < $sliders.length - 1) {
+          currentIndex++;
+          showSlider(currentIndex);
+        }
+      });
+    }
+  );
 });
